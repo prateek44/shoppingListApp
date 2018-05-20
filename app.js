383521e0aaa52e6b44a92426539b8345f8acdf9d
prateek44 <44beastmodeon@gmail.com>
2018-05-20 15:58:25 +0530

    shoppingListApp to add new items to the list ...

--- app.js
+++ app.js
@@ -9,10 +9,13 @@
       {name:"milk",qty:"2"},{name:"oats",qty:"4"},{name:"cookies",qty:"5"},{name:"nuts",qty:"3"}
     ];
     $scope.shoppingListItems=shoppingList;
-    
 
+    $scope.len=$scope.shoppingListItems.length;
+    console.log("recently added...",$scope.shoppingListItems[$scope.len-1].name);
+    console.log("number of items in list",$scope.len);
     $scope.addToList=function()
     {
+      console.log("number of watchers #",$scope.$$watchersCount);
       var newItemObject={
         name:$scope.newItem,
         qty:$scope.newQuantity
@@ -20,7 +23,10 @@
       console.log("adding items to list...");
       console.log("",newItemObject);
       shoppingList.push(newItemObject);
+      console.log("recently added...",$scope.shoppingListItems[$scope.len-1].name);
       console.log("item successfully added...")
+
     };
+    console.log("number of watchers #",$scope.$$watchersCount);
   }
 })();
